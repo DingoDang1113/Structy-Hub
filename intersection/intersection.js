ee8e0e7258dc84c21fe6894c2b682a4d400add54
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-03-23 10:16:18 -0700

    Solved 'intersection' (.js)

--- intersection/intersection.js
+++ intersection/intersection.js
@@ -1,8 +1,26 @@
 const intersection = (a, b) => {
   // todo
-  return a.filter((ele) => 
-    b.includes(ele)
-  )
+  const items = new Set();
+  const result = [];
+  
+  //n iteration -> load a array into set (unique value)
+  for (let item of a) {
+    items.add(item);
+  }
+  
+  // m iteration -> check if each element of b array is in items Set 
+  for (let ele of b) {
+     if (items.has(ele) ) {
+       result.push(ele);
+     }
+  }
+  
+  return result;
+  
+  // O(n * m) - not the best efficient way
+  // return a.filter(ele => 
+  //   b.includes(ele)
+  // )
 };
 ​
 module.exports = {
