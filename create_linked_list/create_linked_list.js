e6f5559d2be08d8b419d8d35f5c7c93e93115ba1
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-08 12:47:54 -0700

    Solved 'create linked list' (.js)

--- create_linked_list/create_linked_list.js
+++ create_linked_list/create_linked_list.js
@@ -7,27 +7,37 @@ class Node {
 
 
 const createLinkedList = (values) => {
-  // ITERATIVE
-
-
-  // create the head of linkedList for .next 
-  const dummyHead = new Node(null);
-  let tail = dummyHead;
-
-
-  // iterate through the array to link the new Node(value) to tail 
-  for (let val of values) {
-    tail.next = new Node(val);
-    // move tail to its next node
-    tail = tail.next;
-  }
-
-
-  return dummyHead.next;
-
-
-  
+  // RECURSIVE - 1 
+  // base case to stop recursion
+  if (values.length === 0) return null;
+  // set head with first element in arr
+  const head = new Node(values[0]);
+  //append next node thru head.next
+  head.next = createLinkedList(values.slice(1)) 
+  // return head 
+  return head;
 };
+
+
+// const createLinkedList = (values) => {
+//   // ITERATIVE
+
+
+//   // create the head of linkedList for .next 
+//   const dummyHead = new Node(null);
+//   let tail = dummyHead;
+
+
+//   // iterate through the array to append the new Node(value) to tail 
+//   for (let val of values) {
+//     tail.next = new Node(val);  // append the new node to current node
+//     // move tail to its next node
+//     tail = tail.next;
+//   }
+
+
+//   return dummyHead.next;
+// };
 
 
 module.exports = {
