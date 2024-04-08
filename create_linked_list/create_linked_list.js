e1e6588837e1379f8249e110d097087f3a096092
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-08 12:50:59 -0700

    Solved 'create linked list' (.js)

--- create_linked_list/create_linked_list.js
+++ create_linked_list/create_linked_list.js
@@ -6,17 +6,32 @@ class Node {
 }
 
 
-const createLinkedList = (values) => {
-  // RECURSIVE - 1 
+const createLinkedList = (values, i = 0) => {
+  // RECURSIVE Solution 2 (best) 
   // base case to stop recursion
-  if (values.length === 0) return null;
-  // set head with first element in arr
-  const head = new Node(values[0]);
+  if (i === values.length) return null;
+  // create node for each element in the array
+  const head = new Node(values[i]);
   //append next node thru head.next
-  head.next = createLinkedList(values.slice(1)) 
+  head.next = createLinkedList(values, i + 1) // use i as a tracker of index
   // return head 
   return head;
 };
+
+
+
+
+// const createLinkedList = (values) => {
+//   // RECURSIVE Solution 1 (not the best option due to n^2 time complexity) 
+//   // base case to stop recursion
+//   if (values.length === 0) return null;
+//   // set head with first element in arr
+//   const head = new Node(values[0]);
+//   //append next node thru head.next
+//   head.next = createLinkedList(values.slice(1)) // slice create duplicated array 
+//   // return head 
+//   return head;
+// };
 
 
 // const createLinkedList = (values) => {
