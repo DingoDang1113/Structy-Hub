2de19af82b3510bb37603652647f289cc5563e26
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-11 16:31:18 -0700

    Solved 'sum list' (.js)

--- sum_list/sum_list.js
+++ sum_list/sum_list.js
@@ -1,32 +1,26 @@
-
 // class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
 // }
-​
+
+
 const sumList = (head) => {
-  // todo
-// ITERATIVE 
-//   let total = 0;
-//   let current = head;
-  
-//   while (current !== null) {
-//     total += current.val;
-//     current = current.next;
-//   }
-  
-//   return total;
-​
-// RECURSION
-  // let total = 0;    No need to declare total as val + sumList(head.next)
-  
-  if (head === null) return 0;
-  return head.val + sumList(head.next);
-  
-  // return total;
+  // Iterative 
+  let total = 0; 
+  let current = head; 
+
+
+  while(current !== null) {
+    total += current.val; 
+    current = current.next;
+  }
+  return total;
 };
-​
+
+
 module.exports = {
-  sumList,
+  sumList,
+};
+
