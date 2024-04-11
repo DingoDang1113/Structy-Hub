1107300565a75dfbfe0acd22789778f8158e51cd
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-11 16:40:41 -0700

    Solved 'linked list find' (.js)

--- linked_list_find/linked_list_find.js
+++ linked_list_find/linked_list_find.js
@@ -1,27 +1,33 @@
-
 // class Node {
-//   constructor(val) {
-//     this.val = val;
-//     this.next = null;
-//   }
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
 // }
-​
 const linkedListFind = (head, target) => {
-  // todo
-  // RECURSIVE
-  if (head && head.val === target) return true;
-  if (head === null) return false;
-  // head.val === target;
-  return linkedListFind(head.next, target); // remember to add all params
-  
-  //ITERATIVE - better solution with space(1)
-  // let current = head;
-  // while (current !== null ) {
-  //   if(current.val === target ) {
-  //     return true;
-  //   } 
-  //   current = current.next;
-  //   
-  // }
-  // return false;
-  
+  //RECURSION 
+  if (head === null) return false; 
+
+
+  if (head.val === target) return true;
+
+
+  return linkedListFind(head.next, target);
+  
+  
+}
+// const linkedListFind = (head, target) => {
+//   // ITERATIVE - time: O(n), space: 0(n)
+//   let current = head; 
+//   while( current !== null) {
+//     if (current.val === target) return true;
+//     current = current.next;
+//   }
+//   return false;
+// };
+
+
+module.exports = {
+  linkedListFind,
+};
+
