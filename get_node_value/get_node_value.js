f39a3817891c0ffa8776a8f1d00b258fedc94626
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-11 17:03:01 -0700

    Solved 'get node value' (.js)

--- get_node_value/get_node_value.js
+++ get_node_value/get_node_value.js
@@ -6,24 +6,35 @@
 // }
 
 
+// Situation 1 -> index exits  => return node.val
+// Situation 2 -> index does not exits => return null;
+
+
 const getNodeValue = (head, index) => {
-  // ITERATIVE 
-  let count = 0; 
-  let current = head; 
+  //RECURSION 
+  if (head === null) return null; 
+  if (index === 0) return head.val; 
 
 
-  while( current !== null) {
-    if (count === index) {
-      return current.val;
-    } 
-    count += 1; 
-    current = current.next;
-  }
-  return null;
+  return getNodeValue(head.next, index -1);
+}
 
 
-  
-};
+// const getNodeValue = (head, index) => {
+//   // ITERATIVE -> time: O(n), space: O(1) 
+//   let count = 0; 
+//   let current = head; 
+
+
+//   while( current !== null) {
+//     if (count === index) {
+//       return current.val;
+//     } 
+//     count += 1; 
+//     current = current.next;
+//   }
+//   return null;
+// };
 
 
 module.exports = {
