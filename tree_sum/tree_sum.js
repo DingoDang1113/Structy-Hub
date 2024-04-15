8a66b55d372ff1bb8cc67baae5a88a6066f358f2
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-15 11:37:12 -0700

    Solved 'tree sum' (.js)

--- tree_sum/tree_sum.js
+++ tree_sum/tree_sum.js
@@ -8,24 +8,48 @@
 
 
 const treeSum = (root) => {
-  // Iterative - DFS 
-  let sum = 0; 
-  const stack = [root];
+  // Recursion 
+  if (root === null) return 0;
+  return root.val + treeSum(root.left) + treeSum(root.right); 
+}
 
 
-  while (stack.length > 0) {
-    let current = stack.pop(); 
-    if (current !== null) {
-      sum += current.val;   
-      if(current.left) stack.push(current.left);
-      if(current.right) stack.push(current.right);
-    }
-  }
 
 
-  return sum;
-  
-};
+// const treeSum = (root) => {
+//   // Iterative - DFS 
+//   let sum = 0; 
+//   const stack = [root];
+
+
+//   while (stack.length > 0) {
+//     let current = stack.pop();   // start with end of arr
+//     if (current !== null) {
+//       sum += current.val;   
+//       if(current.left) stack.push(current.left);
+//       if(current.right) stack.push(current.right);
+//     }
+//   }
+//   return sum;
+// };
+
+
+// const treeSum = (root) => {
+//   // Iterative - BFS 
+//   let sum = 0; 
+//   const queue = [root];
+
+
+//   while (queue.length > 0) {
+//     let current = queue.shift();  // start with head of arr
+//     if (current !== null) {
+//       sum += current.val;   
+//       if(current.left) queue.push(current.left);
+//       if(current.right) queue.push(current.right);
+//     }
+//   }
+//   return sum;
+// };
 
 
 module.exports = {
