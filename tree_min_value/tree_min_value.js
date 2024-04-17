9ac73cbe15980b6d6f0c43e02226b66163851c85
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-17 10:37:13 -0700

    Solved 'tree min value' (.js)

--- tree_min_value/tree_min_value.js
+++ tree_min_value/tree_min_value.js
@@ -7,19 +7,38 @@
 // }
 
 
+// const treeMinValue = (root) => {
+//   // Iterative - DFS 
+//   const stack = [root]; 
+//   let minVal = root.val; 
+
+
+//   while(stack.length > 0) {
+//     let current = stack.pop();
+//     if(minVal > current.val) {
+//       minVal = current.val;
+//     }
+//     if(current.left) stack.push(current.left); 
+//     if(current.right) stack.push(current.right);
+//   }
+//   return minVal;
+  
+// };
+
+
 const treeMinValue = (root) => {
-  // Iterative - DFS 
-  const stack = [root]; 
+  // Iterative - BFS 
+  const queue = [root]; 
   let minVal = root.val; 
 
 
-  while(stack.length > 0) {
-    let current = stack.pop();
+  while(queue.length > 0) {
+    let current = queue.shift();
     if(minVal > current.val) {
       minVal = current.val;
     }
-    if(current.left) stack.push(current.left); 
-    if(current.right) stack.push(current.right);
+    if(current.left) queue.push(current.left); 
+    if(current.right) queue.push(current.right);
   }
   return minVal;
   
