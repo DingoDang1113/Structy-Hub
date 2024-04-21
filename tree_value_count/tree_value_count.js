75a713872755587cadc6f3e84669d67c587843ea
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-21 08:45:34 -0700

    Solved 'tree value count' (.js)

--- tree_value_count/tree_value_count.js
+++ tree_value_count/tree_value_count.js
@@ -7,15 +7,41 @@
 // }
 
 
+// const treeValueCount = (root, target) => {
+//   // ITERATIVE - DFS 
+//   if (root === null) return 0;
+//   const stack = [root]; 
+//   let count = 0; 
+
+
+//   while (stack.length > 0) {
+//     let current = stack.pop(); 
+
+
+//     if(current.val === target) {
+//       count += 1;
+//     }
+
+
+//     if(current.left) stack.push(current.left);
+//     if(current.right) stack.push(current.right);
+    
+//   }
+
+
+//   return count;
+// };
+
+
 const treeValueCount = (root, target) => {
-  // ITERATIVE - DFS 
+  // ITERATIVE - BFS 
   if (root === null) return 0;
-  const stack = [root]; 
+  const queue = [root]; 
   let count = 0; 
 
 
-  while (stack.length > 0) {
-    let current = stack.pop(); 
+  while (queue.length > 0) {
+    let current = queue.shift(); 
 
 
     if(current.val === target) {
@@ -23,17 +49,16 @@ const treeValueCount = (root, target) => {
     }
 
 
-    if(current.left) stack.push(current.left);
-    if(current.right) stack.push(current.right);
+    if(current.left) queue.push(current.left);
+    if(current.right) queue.push(current.right);
     
   }
 
 
   return count;
-
-
-  
 };
+
+
 
 
 module.exports = {
