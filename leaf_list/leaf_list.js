476f21155fc160d6db4be7dac3db23e9d56f035e
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-04-29 10:41:17 -0700

    Solved 'leaf list' (.js)

---
+++ leaf_list/leaf_list.js
@@ -0,0 +1,39 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
+//   }
+// }
+
+
+const leafList = (root) => {
+  // RECURSION 
+  if (root === null) return [];
+  if (root.left === null && root.right === null) return [root.val];
+
+
+  return [...leafList(root.left), ...leafList(root.right)];
+
+
+  
+}
+
+
+// const leafList = (root) => {
+//   // iterative - BFS 
+
+
+//   const queue = [root]; 
+  
+  
+
+
+  
+// };
+
+
+module.exports = {
+  leafList,
+};
+
