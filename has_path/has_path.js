f0b9021c5023ae839047227a3128cf373f6aaf6b
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-05-01 16:30:35 -0700

    Solved 'has path' (.js)

--- has_path/has_path.js
+++ has_path/has_path.js
@@ -1,18 +1,10 @@
 const hasPath = (graph, src, dst) => {
   // ITERATIVE - DFS 
-
-
   const stack = [src];
-
-
   while( stack.length > 0 ) {
     let current = stack.pop(); 
-    console.log(current)
-
-
     if (current === dst) return true;
-
-
+    
     for (let neighbor of graph[current]) {
       stack.push( neighbor )
     }
