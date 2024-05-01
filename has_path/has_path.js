ce45c94b81e945deb6de418057b2a80a87293491
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-05-01 16:38:21 -0700

    Solved 'has path' (.js)

--- has_path/has_path.js
+++ has_path/has_path.js
@@ -1,19 +1,45 @@
-const hasPath = (graph, src, dst) => {
-  // ITERATIVE - DFS 
-  const stack = [src];
-  while( stack.length > 0 ) {
-    let current = stack.pop(); 
-    if (current === dst) return true;
+// const hasPath = (graph, src, dst) => {
+//   // RECURSION 
+//   if(src === dst) return true;
+//   if(src === null) return false;
+//   console.log(src);
+
+
+//   for(let neighbor of graph[src]) {
+//     hasPath(graph, neighbor, dst);
+//   };
+// }
+
+
+// const hasPath = (graph, src, dst) => {
+//   // ITERATIVE - DFS 
+//   const stack = [src];
+//   while( stack.length > 0 ) {
+//     let current = stack.pop(); 
+//     if (current === dst) return true;
     
+//     for (let neighbor of graph[current]) {
+//       stack.push( neighbor )
+//     }
+//   }
+
+
+//   return false;
+// };
+
+
+const hasPath = (graph, src, dst) => {
+  // ITERATIVE - BFS 
+  const queue = [src]; 
+  while(queue.length > 0) {
+    let current = queue.shift();
+    if (current === dst) return true;
     for (let neighbor of graph[current]) {
-      stack.push( neighbor )
-    }
+      queue.push(neighbor);
   }
-
-
+  }
   return false;
-  
-};
+}
 
 
 module.exports = {
