8439a0ae3a1cf4c5c862877e09905def3e9b0fe1
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-06-26 15:42:10 -0700

    Solved 'sum possible' (.js)

--- sum_possible/sum_possible.js
+++ sum_possible/sum_possible.js
@@ -10,32 +10,57 @@ sumPossible(2017, [4, 2, 10]); // -> false
 sumPossible(13, [3, 5]); // -> true
 sumPossible(10, [4, 5, 7]); // -> true
 const sumPossible = (amount, numbers, memo = {} ) => {
-  // basecase 1 - amount = 0 as null can achieve this  
-  if (amount === 0) return true;
-  // basecase 2 - amount < 0 (can't be negative)
-  if (amount < 0 ) return false; 
-  // basecase 3 - amount is in memo, use that value to avoid repetitive calculation
-  if (amount in memo) return memo[amount]; 
+  // create a boolean array to store results of sub problems
+  const dp = Array(amount + 1).fill(false); 
+  dp[0] = true; // basecase 0 is always achieveable 
 
 
-  for (let num of numbers) {
-    if (sumPossible(amount - num, numbers, memo) === true) {
-      memo[amount] = true;
-      return true;
+  // Iterate number from 1 to amount, for each i iterate through num in Numbers
+  for (let i = 1; i <= amount; i++) {
+    for (let num of numbers) {
+      if (i - num >= 0 && dp[i - num]) {
+        dp[i] = true;
+        break;
+      }
     }
   }
-  // console.log(memo)
-  memo[amount] = false;
-  return false;
+  return dp[amount];
 
 
-};
+  // console.log(dp[amount])
+  
+}
 
 
-sumPossible(8, [5, 12, 4])
+
+
+// const sumPossible = (amount, numbers, memo = {} ) => {
+//   // basecase 1 - amount = 0 as null can achieve this  
+//   if (amount === 0) return true;
+//   // basecase 2 - amount < 0 (can't be negative)
+//   if (amount < 0 ) return false; 
+//   // basecase 3 - amount is in memo, use that value to avoid repetitive calculation
+//   if (amount in memo) return memo[amount]; 
+
+
+//   for (let num of numbers) {
+//     if (sumPossible(amount - num, numbers, memo) === true) {
+//       memo[amount] = true;
+//       return true;
+//     }
+//   }
+//   memo[amount] = false;
+//   return false;
+
+
+//   // console.log(memo)
+
+
+// };
+
+
+// sumPossible(8, [5, 12, 4])
 
 
 module.exports = {
   sumPossible,
-};
-
