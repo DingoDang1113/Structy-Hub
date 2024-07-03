a54556fe867f7e8da2f2daccf4f29aa117132873
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-07-03 15:41:24 -0700

    Solved 'counting change' (.js)

---
+++ counting_change/counting_change.js
@@ -0,0 +1,43 @@
+For example,
+
+
+countingChange(4, [1,2,3]) -> 4
+
+
+There are four different ways to make an amount of 4:
+
+
+1. 1 + 1 + 1 + 1
+2. 1 + 1 + 2
+3. 1 + 3
+4. 2 + 2
+countingChange(4, [1, 2, 3]); // -> 4
+countingChange(8, [1, 2, 3]); // -> 10
+countingChange(24, [5, 7, 3]); // -> 5
+countingChange(13, [2, 6, 12, 10]); // -> 0
+countingChange(512, [1, 5, 10, 25]); // -> 20119
+countingChange(1000, [1, 5, 10, 25]); // -> 142511
+countingChange(240, [1, 2, 3, 4, 5, 6, 7, 8, 9]); // -> 1525987916
+const countingChange = (amount, coins,i=0, memo={}) => {
+  if (amount === 0) return 1;
+  const coin = coins[i];
+  const key = amount + ',' + i;
+  if (key in memo) return memo[key];
+
+
+  let total = 0;
+  for(let qty=0; qty * coin <= amount; qty +=1) {
+    const remainder = amount - (qty*coin);
+    total += countingChange(remainder, coins, i+1, memo);
+  }
+  memo[key] = total;
+  return total;
+
+
+};
+
+
+module.exports = {
+  countingChange,
+};
+
