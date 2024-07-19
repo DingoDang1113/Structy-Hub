a13396dfa9f946b1849a3a0d0cb397ff1f4e00f1
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-07-19 12:46:02 -0700

    Solved 'knightly number' (.js)

---
+++ knightly_number/knightly_number.js
@@ -0,0 +1,40 @@
+const knightlyNumber = (n, m, kr, kc, pr, pc, memo = {}) => {
+  const pos = m + "," + kr + "," + kc;
+  if (pos in memo) return memo[pos];
+  //check for in bounds
+  if (kr < 0 || kr >= n || kc < 0 || kc >= n) return 0;
+  // if no steps left
+  if ( m === 0) return (kr === pr && kc === pc) ? 1 : 0;
+
+
+  const moves = [
+    [ kr + 2, kc + 1 ],
+    [ kr - 2, kc + 1 ],
+    [ kr + 2, kc - 1 ],
+    [ kr - 2, kc - 1 ],
+    [ kr + 1, kc + 2 ],
+    [ kr - 1, kc + 2 ],
+    [ kr + 1, kc - 2 ],
+    [ kr - 1, kc - 2 ],
+  ] ;
+
+
+  let count = 0;
+  for (let move of moves) {
+    const [nextR, nextC] = move;
+    count += knightlyNumber(n, m - 1, nextR, nextC, pr, pc, memo);
+  }
+
+
+  memo[pos] = count;
+  // return count;
+  return memo[pos];
+
+
+};
+
+
+module.exports = {
+  knightlyNumber
+};
+
