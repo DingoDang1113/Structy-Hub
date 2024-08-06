fd9fc279a439835b8000427eabbfd522c442195a
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-08-06 15:17:14 -0700

    Solved 'substituting synonyms' (.js)

--- substituting_synonyms/substituting_synonyms.js
+++ substituting_synonyms/substituting_synonyms.js
@@ -1,47 +1,40 @@
 const substituteSynonyms = (sentence, synonyms) => {
+  // split sentence by words 
   const words = sentence.split(' ');
+  // helper function to recursively populate the sentence
   const final = generate(words, synonyms);
-  return final.map(sub => sub.join(' '));
-
-
+  return final.map(sub => sub.join(' '))
 };
 
 
 const generate = (words, synonyms) => {
-  if (words.length === 0) return [[]];
+  //basecase words is empty 
+  if (words.length === 0) return [[]]; 
 
 
-  const firstWord = words[0];
+  const firstWord = words[0]; 
   const remaining = words.slice(1);
 
 
+  // if first word is in synonyms, iterate every word to sentence
   if (firstWord in synonyms) {
-    const result = [];
-    const subArrays = generate(remaining, synonyms);
+    const result = []; 
+    const subarrays = generate(remaining, synonyms);
     for (let synonym of synonyms[firstWord]) {
-      for (let subArray of subArrays) {
-        result.push([synonym, ...subArray]) 
+      for (let subarray of subarrays) {
+        result.push([synonym, ...subarray])
       }
     }
-    
-    
     return result;
+    
   } else {
-    
-    const result = [];
-    const subArrays = generate(remaining, synonyms);
-
-
-    for (let subArray of subArrays) {
-      result.push([firstWord,...subArray]);
+    const result = []; 
+    const subarrays = generate(remaining, synonyms);
+    for (let subarray of subarrays) {
+      result.push([firstWord, ...subarray])
     }
-    
     return result;
-    
   }
-
-
-  
 
 
   
