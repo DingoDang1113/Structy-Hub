8ae0d0d91dd55b28d6d80811cfcf3073892c576a
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-08-08 12:17:35 -0700

    Solved 'middle value' (.js)

---
+++ middle_value/middle_value.js
@@ -0,0 +1,26 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.next = null;
+//   }
+// }
+
+
+const middleValue = (head) => {
+  // declare an array variable that stores all the values from node 
+  const result = []; 
+  let current = head; 
+  while (current) {
+    result.push(current.val);
+    current = current.next;
+  }
+  // return the middle value of array 
+  const idx = result.length % 2 === 0 ? result.length / 2 :(result.length-1) /2 
+  return result[idx]
+};
+
+
+module.exports = {
+  middleValue,
+};
+
