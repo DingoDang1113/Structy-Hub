477c47b6dd8518a23cc2c755c060fe65b833d964
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-08-19 15:38:15 -0700

    Solved 'lefty nodes' (.js)

--- lefty_nodes/lefty_nodes.js
+++ lefty_nodes/lefty_nodes.js
@@ -1,64 +1,45 @@
+// class Node {
+//   constructor(val) {
+//     this.val = val;
+//     this.left = null;
+//     this.right = null;
 //   }
 // }
-
-
-// const leftyNodes = (root) => {
-//   if (root === null ) return [];
-
-
-//   const values = [];
-//   const stack = [{node: root, level: 0}];
-
-
-//   while(stack.length > 0) {
-//     const {node, level} = stack.pop();
-    
-//     if (values.length === level) {
-//       values.push(node.val);
-//     }
-
-
-//     if(node.right !== null) {
-//       stack.push({node: node.right, level: level + 1})
-//     };
-
-
-//     if(node.left !== null ) {
-//       stack.push({node: node.left, level: level + 1})
-//     };
-    
-    
-//   }
-
-
-//   return values;
-// }
-
-
 
 
 const leftyNodes = (root) => {
-  // dfs - will traverse the tree in depth first 
-  
-  const values = [];
-  // CLOSURE
-  const traverse = (root, level) => {
-      if (root === null) return ;
-      // make sure the first node of the level will get to pushed 
-      if (values.length === level ) {
-        values.push(root.val);
-      }
-      //make sure the trees are traversed from left to right 
-      traverse(root.left, level + 1);
-      traverse(root.right, level + 1);
+  const values = []; 
+
+
+  // traverse thru nodes 
+  const traverse = (node, level) => {
+    if (node === null ) return;
+
+
+    // 
+    if (values[level] === undefined) {
+      values.push(node.val);
+    }
+
+
+    // make sure nodes traverse from left to right 
+    traverse(node.left, level + 1);
+    traverse(node.right, level + 1);
     
   };
-  traverse(root,0);
-  return values; 
-    
-  }
 
 
+  traverse(root, 0);
+
+
+  return values;
+
+
+  
+
+
+  
+};
 
 
 module.exports = {
