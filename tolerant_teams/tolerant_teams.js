bca7d348e85e15ed6c45ee7b0a26d253670b72c9
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-08-29 15:21:25 -0700

    Solved 'tolerant teams' (.js)

--- tolerant_teams/tolerant_teams.js
+++ tolerant_teams/tolerant_teams.js
@@ -1,46 +1,42 @@
 const tolerantTeams = (rivalries) => {
-  // turn pairs into adjacency list structure 
+  // turn edges into adjacent list 
   const graph = buildGraph(rivalries);
-
   const visited = {};
 
 
   for (let rivary in graph) {
-    if (!(rivary in visited) && !check(rivary, graph, visited, false)) {
+    if (!(rivary in visited) && !checkRivary(rivary,graph,visited, false)) {
       return false;
     }
+    
   }
-
-
-  return true;  
-};
-
-
-const check = (node, graph, visited, teamColor) => {
-  if(node in visited) return visited[node]=== teamColor;
-  visited[node] = teamColor;
-
-
-  for(let neighbor of graph[node]) {
-    if (check(neighbor, graph, visited, !teamColor) === false) {
-      return false;
-    }
-  }
-
-
   return true;
-
-
   
 };
 
 
+const checkRivary = (node, graph, visited, teamColor) => {
+  if (node in visited) return visited[node] === teamColor;
+  visited[node] = teamColor;
+
+
+  for (let neighbor of graph[node]) {
+    if (checkRivary(neighbor, graph, visited, !teamColor) === false) {
+      return false;
+    }
+  }
+  return true;
+}
+
+
+
+
 const buildGraph = (edges) => {
-  const graph = {}; 
-
-
-  for(let edge of edges) {
-    const [a, b] = edge;
+  
+  const graph = {};
+  
+  for (let edge of edges ) {
+    const[a,b] = edge;
     if (!(a in graph)) graph[a] = [];
     if (!(b in graph)) graph[b] = [];
     graph[a].push(b);
@@ -49,6 +45,7 @@ const buildGraph = (edges) => {
 
 
   return graph;
+  
 }
 
 
