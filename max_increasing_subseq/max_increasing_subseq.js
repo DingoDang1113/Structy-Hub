6bc244f961d2c7ec4ff691f0fe8a673330a8e908
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-09-17 16:54:24 -0700

    Solved 'max increasing subseq' (.js)

--- max_increasing_subseq/max_increasing_subseq.js
+++ max_increasing_subseq/max_increasing_subseq.js
@@ -1,24 +1,26 @@
-const maxIncreasingSubseq = (numbers, i = 0, prevNum = -Infinity, memo = {}) => {
+const maxIncreasingSubseq = (numbers, i = 0, prevNum = -Infinity, memo={}) => {
   const key = i + "," + prevNum;
   if (key in memo) return memo[key];
-  if( i === numbers.length) return 0; 
-
-
-  const current = numbers[i];
-  const option = [];
+  
+  if (i === numbers.length ) return 0; 
+  const current = numbers[i]; 
+  const options = [];
 
 
   const notTakeCurrent = maxIncreasingSubseq(numbers, i + 1, prevNum, memo);
-  option.push(notTakeCurrent);
-  
+  options.push( notTakeCurrent);
+
+
   if (current > prevNum) {
-    const takeCurrent = 1 + maxIncreasingSubseq(numbers, i + 1, current, memo); 
-    option.push(takeCurrent);
-  };
+    const takeCurrent = 1 + maxIncreasingSubseq(numbers, i + 1, current, memo);
+    options.push(takeCurrent)
+  }
 
 
-  memo[key] =  Math.max(...option);
+  // console.log(options)
+  memo[key] = Math.max(...options);
   return memo[key];
+  
   
 };
 
