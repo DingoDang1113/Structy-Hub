3543c63e971bf2ab255bd2f4070dca0aa0d960ba
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-09-27 14:57:50 -0700

    Solved 'rare routing' (.js)

--- rare_routing/rare_routing.js
+++ rare_routing/rare_routing.js
@@ -1,55 +1,53 @@
 const rareRouting = (n, roads) => {
+  // generate graph 
   const graph = generateGraph(n, roads);
 
 
   const visited = new Set();
+  const valid = traverseCities("0", graph, visited, null);
+
+  // iterate thru every node of the graph to check if any node has been visited before 
+    // if has been visited, return false (means the route has been explored before)
 
 
-  const valid = traverseCities('0', graph, visited, null);
-
-
+  // return the validity of graph, and and check the size of the visited 
   return valid && visited.size === n;
-
-
-  
 };
-
-
 
 
 const traverseCities = (city, graph, visited, prevCity) => {
-  if (visited.has(city)) return false; 
-  visited.add(city);
+  if (visited.has(city)) return false;
+  visited.add(city); 
 
 
-  for (let neighbor of graph[city]) {
-    if (neighbor !== prevCity && traverseCities(neighbor, graph, visited, city) === false) {
+  for(let neighbor of graph[city]) {
+    if(neighbor !== prevCity && traverseCities(neighbor, graph, visited, city) === false){
       return false;
     }
   }
-
-
   return true;
-  
-};
+}
 
 
 const generateGraph = (n, roads) => {
-  const graph = {}; 
 
 
-  for (let city = 0; city < n; city += 1) {
+  const graph = {};
+
+
+  for (let city = 0; city < n ; city += 1) {
     graph[city] = [];
   };
 
 
   for (let road of roads) {
-    const [a,b] = road; 
+    const [a, b] = road
     graph[a].push(String(b));
     graph[b].push(String(a));
-  };
 
 
+    
+  }
   return graph;
   
 }
