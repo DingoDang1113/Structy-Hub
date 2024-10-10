7a0b4ae2bb349cf4741cc7f76d6076da752e6505
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-10-09 17:23:09 -0700

    Solved 'positioning plants' (.js)

--- positioning_plants/positioning_plants.js
+++ positioning_plants/positioning_plants.js
@@ -1,16 +1,17 @@
 const positioningPlants = (costs, pos = 0, lastPos = null, memo = {}) => {
   const key = pos + ',' + lastPos;
   if (key in memo) return memo[key];
-  
-  if (pos === costs.length) return 0; 
+  // check if pos is still inbound
+  if (pos === costs.length) return 0;
 
 
   let min = Infinity; 
 
 
+  // iterate the rest of plants based on current plant pos
   for (let plant = 0; plant < costs[pos].length; plant += 1) {
     if (plant !== lastPos) {
-      const cost = costs[pos][plant] + positioningPlants(costs, pos + 1, plant, memo);
+      const cost = costs[pos][plant] + positioningPlants(costs, pos + 1, plant, memo );
       min = Math.min(min, cost);
     }
   }
