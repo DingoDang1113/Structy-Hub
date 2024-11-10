efbd2a2e68b2e069e1f739b15a1da5fd4627d398
Shali Peng <73029929+DingoDang1113@users.noreply.github.com>
2024-11-09 19:56:31 -0800

    Solved 'most frequent char' (.js)

--- most_frequent_char/most_frequent_char.js
+++ most_frequent_char/most_frequent_char.js
@@ -1,32 +1,31 @@
-
 const mostFrequentChar = (s) => {
-  // todo
-  
-  const count = {};
-  
-  for (let char of s) {
-    if (!(count[char])) {
-        count[char] = 1;
-        } else {
-          count[char] += 1;
-        }
+  // todo
+  
+  const count = {};
+  
+  for (let char of s) {
+    if (!(count[char])) {
+        count[char] = 0;
+        } 
+        
+    count[char] += 1;
+        
   }
-  
-  let max = 0;
-  let result = "";
-  for (let char in count) {
-    if (count[char] > max) {
-      max = count[char];
-      result = char;
-    } 
+  
+  let result = "";
+  for (let char in count) {
+    if (result === "" ||count[char] > count[result]) {
+      result = char;
+    } 
   }
-  
-  return result;
-  
-  
+  
+  return result;
+  
+  
 };
-​
+
+
 module.exports = {
-  mostFrequentChar,
+  mostFrequentChar,
 };
-​
+
